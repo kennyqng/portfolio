2ecdb9a6f6c80b67c63e16f0309fa41079b6d216
hadooken <kennyqng@gmail.com>
2020-08-09 18:31:28 -0700

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,14 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer className="footer bg-info" id="footer-trim"></footer>
-      <footer className="footer bg-dark" id="footer">
-        <div className="containter text-center">
-          <small className="text-light">Copyright&copy; kennyqng</small>
-        </div>
-      </footer>
+      <div className="footer-space">
+        <footer className="footer bg-info" id="footer-trim"></footer>
+        <footer className="footer bg-dark" id="footer">
+          <div className="containter text-center">
+            <small className="text-light">Copyright&copy; kennyqng</small>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
