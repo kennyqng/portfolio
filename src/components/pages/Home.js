6bf77000f2b532cbf6eae3e4076575ebefccf04c
hadooken <kennyqng@gmail.com>
2020-09-26 23:46:56 -0700

    import clean up in Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import profile from "../../Assets/profile.JPG";
-import { Link, useLocation } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 function Home() {
   return (
